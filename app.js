98eceea9b57368fe8c8a6a7f9f163db1461742fc
Ruben Maldonado <ruben@maldonadotena.com>
2020-02-21 12:37:55 -0600

    add google auth

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 
+// Passport Conifg
+require('./config/passport')(passport);
 
 //Routes
+const auth = require('./routes/auth')
+
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
 
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
